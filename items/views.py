c077b9f8fbc389f7848b75d37a1e3f8eeefda78c
DariuusPC <dariuuslml@gmail.com>
2023-07-27 21:25:02 -0600

    Creando las primeras vistas y templates html

---
+++ items/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+    
+def signin(request):
+    return render(request, 'signin.html')
+    
